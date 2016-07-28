49504d8f081153d6eba00adf5883504eb5de9c1f
Evgeniy Podgaetskiy <epodgaetskiy@gmail.com>
2016-07-28 17:46:02 +0300

    fix mobile

--- js/common.js
+++ js/common.js
@@ -29,5 +29,57 @@ $(function() {
   		autoplaySpeed: 8000,
   		speed: 1000
 	});
+	//function click for popup
+	function clickPopup(btn, close, popup) {
+
+		btn.click(function(e){
+			e.preventDefault();
+
+			popup.addClass("show-popup");
+
+		})
+
+		close.click(function(e){
+			e.preventDefault();
+
+			popup.removeClass("show-popup");
+
+		})
+
+	}
+	//callback
+	var btnCallback = $(".call-me");
+		popupCallback = $(".callback-popup");
+		closePopupCallback =$(".callback-popup .icon-close");
+
+	clickPopup(btnCallback, closePopupCallback, popupCallback);
+		
+	//menu open 
+	var btnMenu = $(".header__icon-menu");
+		popupMenu = $(".menu-popup");
+		closePopupMenu = $(".menu-popup .icon-close");
+
+	clickPopup(btnMenu, closePopupMenu, popupMenu);
+
+	//scroll to id
+	$(".menu-list li a").mPageScroll2id();
+
+	$(".menu-list li a").click(function(){
+		popupMenu.removeClass("show-popup");
+	})
+
+	//read more open 
+	var btnReadMore = $(".read-more");
+		popupReadMore = $(".read-more-popup");
+		closePopupReadMore = $(".read-more-popup .icon-close");
+
+	clickPopup(btnReadMore, closePopupReadMore, popupReadMore);
+
+	//plugin for scroll scroll-pane
+	$(".read-more-popup .container-content").jScrollPane();
+	// $(".jspTrack").css("height", "200px");
+	$(".jspDrag").css("height", "40px");
+
+
 
 });
